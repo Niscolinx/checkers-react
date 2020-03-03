d5713164414eb794f2d6740f467b001e6a5436a8
Niscolinx <munisco12@gmail.com>
2020-03-03 22:48:02 +0100

    update

--- src/ReactCheckers.js
+++ src/ReactCheckers.js
@@ -8,7 +8,6 @@ export class ReactCheckers {
 
     console.log(this.columns)
     console.log(this.columns)
-    console.log(this.columns)
     getCorners(coordinates) {
 
         const col = utils.getColAsInt(this.columns, coordinates);
