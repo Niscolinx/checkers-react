57e3ee9427fc95d3cd3635188d710f5f900fa967
Niscolinx <munisco12@gmail.com>
2020-03-27 23:00:45 +0100

    constructed the column

--- src/ReactCheckers.js
+++ src/ReactCheckers.js
@@ -12,7 +12,6 @@ export class ReactCheckers {
         const col = utils.getColAsInt(this.columns, coordinates);
         const col = utils.getColAsInt(this.columns, coordinates);
         const col = utils.getColAsInt(this.columns, coordinates);
-        const col = utils.getColAsInt(this.columns, coordinates);
         const row = utils.getRowAsInt(coordinates);
 
         const columnLeft  = col -1 >= 0 ? utils.getColAsAlph(this.columns, col - 1) : false;
