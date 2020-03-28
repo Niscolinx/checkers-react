5a4a7ab52dc58351cf2cb60cbe435402236317e7
Niscolinx <munisco12@gmail.com>
2020-03-28 05:45:27 +0100

    decreased the row count

--- src/checkers.js
+++ src/checkers.js
@@ -6,7 +6,6 @@ const checkerOps = (() => {
 	    for (let i = 0; i < num; i++) {
 	    	if (i && i % (size/2) == 0){
 		      	row--;
-		      	row--;
 		      	col = i == size ? 0 : 1;
 		      	col = i == size ? 0 : 1;
 		      }
