52a7f4eb47654bf82adfb7129505a2c547f054fa
Niscolinx <munisco12@gmail.com>
2020-04-02 22:09:50 +0100

    updated the util

--- src/utils.js
+++ src/utils.js
@@ -24,7 +24,6 @@ export function getColAsAlph(columns, columnInt) {
                 let keys = key
                 let keys = key
                 let keys = key
-                let keys = key
             }
             continue;
         }
