44ceb4f90de285b0736c11d78b105d6fef3a1695
Niscolinx <munisco12@gmail.com>
2020-04-03 13:53:35 +0100

    updated player one

--- src/Game.js
+++ src/Game.js
@@ -310,7 +310,6 @@ export class Game extends React.Component {
                     <div className="players-select">
                         <div className="players">
                             <div className="one-player" onClick={() => this.setPlayers(1)}>One Player</div>
-                            <div className="one-player" onClick={() => this.setPlayers(1)}>One Player</div>
                         </div>
                         <div className="players">
                             <div className="two-player" onClick={() => this.setPlayers(2)}>Two Player</div>
