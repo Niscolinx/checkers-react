410624db01decfc904897d71936f093ff927bcd5
Niscolinx <munisco12@gmail.com>
2020-04-06 07:58:39 +0100

    updated the loop

--- src/checkers.js
+++ src/checkers.js
@@ -8,7 +8,6 @@ const checkerOps = (() => {
 	    for (let i = 0; i < num; i++) {
 	    for (let i = 0; i < num; i++) {
 	    for (let i = 0; i < num; i++) {
-	    for (let i = 0; i < num; i++) {
 	    	if (i && i % (size/2) == 0){
 		      	row--;
 		      	col = i == size ? 0 : 1;
