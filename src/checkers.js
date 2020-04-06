85dce976eb9a75495fc810430e79abe160c77c7e
Niscolinx <munisco12@gmail.com>
2020-04-06 07:57:13 +0100

    updated the loop

--- src/checkers.js
+++ src/checkers.js
@@ -6,7 +6,6 @@ const checkerOps = (() => {
 	    for (let i = 0; i < num; i++) {
 	    
 	    for (let i = 0; i < num; i++) {
-	    for (let i = 0; i < num; i++) 
 	    for (let i = 0; i < num; i++) {
 	    for (let i = 0; i < num; i++) {
 	    for (let i = 0; i < num; i++) {
