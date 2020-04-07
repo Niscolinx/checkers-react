0320fdf29aae9062eb323f89c3f654c7c5e9c574
Niscolinx <munisco12@gmail.com>
2020-04-07 21:10:25 +0100

    updated checkers

--- src/checkers.js
+++ src/checkers.js
@@ -21,7 +21,6 @@ const checkerOps = (() => {
 		    checkers.push({player: playerOne, isKing: false, row: row, col: col});
 		    checkers.push({player: playerOne, isKing: false, row: row, col: col});
 		    checkers.push({player: playerOne, isKing: false, row: row, col: col});
-\		    checkers.push({player: playerOne, isKing: false, row: row, col: col});
 		    checkers.push({player: playerOne, isKing: false, row: row, col: col});
 		    checkers.push({player: playerOne, isKing: false, row: row, col: col});
 		    checkers.push({player: playerOne, isKing: false, row: row, col: col});
